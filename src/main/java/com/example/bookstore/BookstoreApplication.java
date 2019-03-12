c1a2549618bf63e4ae20d6c70a64fe039a449fe2
Ville Hotakainen <ville.hotakainen@gmail.com>
2019-03-12 17:51:07 +0200

    implemented UserDetailService and user entities

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -11,10 +11,8 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
-
-
-
-
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 
 @SpringBootApplication
@@ -26,7 +24,7 @@ public static void main(String[] args) {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 			crepository.save(new Category("Novel"));
@@ -35,6 +33,11 @@ public static void main(String[] args) {
 			
 			brepository.save(new Book("A Farewell to Arms", "Ernest Hemingway","1232323-2", 1929, null, crepository.findByName("Novel").get(0)));
 			brepository.save(new Book("Animal Farm", "George Orwell", "2212343-5", 1945, null, crepository.findByName("Novel").get(0)));
+			
+			User user1 = new User("user", "$2a$10$szPIn3pbTKn8QZ3ErILh6O.d80ah5vWr6nLGmWqQ74ymOg3hZkJMe","john@john.com","USER");
+			User user2 = new User("admin", "$2a$10$CHliz9wK/Eojhv/iX0XpO.906H3xBKnpCW26kaE6ymt5vW.l890ma","kate@kate.com", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 	
 		};
 	}
